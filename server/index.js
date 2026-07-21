eca696229d78bc5cb449935dc8ce97a138f4f299
Suresh Jat <sureshjatcode@gmail.com>
2026-07-21 11:20:09 +0530

    Load balancing with nginx

--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
 import express from "express";
+import os from "os";
 
 const app = express();
 
 app.set("trust proxy", 1);
 
 app.get("/api", (req, res) => {
-  res.status(200).send({ success: true, message: "Server is running" });
+  res
+    .status(200)
+    .send({
+      success: true,
+      hostname: os.hostname(),
+      message: "Server is running",
+    });
 });
 app.get("/api/greet", (req, res) => {
-  res.status(200).send({ success: true, message: "Hello from server" });
+  res
+    .status(200)
+    .send({
+      success: true,
+      hostname: os.hostname(),
+      message: "Hello from server",
+    });
 });
 app.get("/api/health", (req, res) => {
-  res.status(200).send({ success: true, message: "Server is Health" });
+  res
+    .status(200)
+    .send({
+      success: true,
+      hostname: os.hostname(),
+      message: "Server is Health",
+    });
 });
 
 app.listen(3000, () => console.log("Server started"));
